77de919acc3b504ace8c329a16c67f7fe54cbf81
Craft055 <craftsman.jvyeu@gmail.com>
2020-03-25 15:02:58 +0900

    ソースコードとテストコードのファイルを自動作成するプログラムを作成。

---
+++ ABC152/A.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def resolve():
+    n, m = map(int, input().split())
+
+    if n == m:
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    resolve()
